e58b3a895815e7058b130220c85c9dc2aaabfbcb
rafal kalisz <rafal.kalisz.dev@gmail.com>
2016-07-26 16:53:40 +0200

    init

---
+++ AustraliaPostSettings.cs
@@ -0,0 +1,11 @@
+﻿using Grand.Core.Configuration;
+
+namespace Grand.Plugin.Shipping.AustraliaPost
+{
+    public class AustraliaPostSettings : ISettings
+    {
+        public string ApiKey { get; set; }
+
+        public decimal AdditionalHandlingCharge { get; set; }
+    }
+}
---
+++ Models/AustraliaPostShippingModel.cs
@@ -0,0 +1,13 @@
+﻿using Grand.Web.Framework;
+
+namespace Grand.Plugin.Shipping.AustraliaPost.Models
+{
+    public class AustraliaPostShippingModel
+    {
+        [NopResourceDisplayName("Plugins.Shipping.AustraliaPost.Fields.ApiKey")]
+        public string ApiKey { get; set; }
+
+        [NopResourceDisplayName("Plugins.Shipping.AustraliaPost.Fields.AdditionalHandlingCharge")]
+        public decimal AdditionalHandlingCharge { get; set; }
+    }
+}
